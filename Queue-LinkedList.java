5c3fba2c24b744be0a8dac2555f8111e7ddfb70f
Abhishek Anand <73717907+AbhishekAnand106001@users.noreply.github.com>
2022-09-17 21:35:23 +0530

    Add files via upload

---
+++ Queue-LinkedList.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+        queue.offer(50);
+        System.out.println(queue);
+        
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.offer(100));
+        System.out.println(queue);
+        
+        System.out.println(queue.peek());
+       
+        
+    }
+}
